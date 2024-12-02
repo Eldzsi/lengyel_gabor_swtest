2cacb25e4a5841576365f5561ec42adccf4eff6a
Eldzsi <113032531+Eldzsi@users.noreply.github.com>
2024-12-02 15:15:55 +0100

    Add Exception handling for negative inputs

--- src/main/java/uni/sw/unit/testing/Fibonacci.java
+++ src/main/java/uni/sw/unit/testing/Fibonacci.java
@@ -2,6 +2,9 @@ package uni.sw.unit.testing;
 
 public class Fibonacci {
     public int getFibonacci(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be a non-negative integer.");
+        }
         if (n == 1 || n == 0) {
             return n;
         }
